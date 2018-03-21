62c5c0c7aa27ee77d7b7ce8f143266c8c92d4d2c
noracashell <noracashell@gmail.com>
2018-03-20 20:49:12 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,35 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Nora";
+let your_name = "Ryan";
+
+if (your_name === "Nora") {
+  renderOutput("That's me!");
+} else if (your_name === "Ryan") {
+  renderOutput("That's Ryan!");
+} else {
+  renderOutput("I must be someone else");
+}
+
+
+function helloWorld(name) {
+  renderOutput("Hello" + name);
+}
+
+helloWorld();
+helloWorld();
+helloWorld();
+
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+helloName("Nora");
+helloName("George");
+helloName("Martha");
+
+function todaysHours(days_hours) {
+  renderOutput(days_hours);
+}
+
+hours.forEach(todaysHours);
